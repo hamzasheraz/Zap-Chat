79f803525b29ab1260ca2df3dcb210efb15540f4
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-11-12 02:11:41 +0500

    Verify email api added

--- pages/api/verifyemail/index.js
+++ pages/api/verifyemail/index.js
@@ -4,10 +4,27 @@ import User from "@/backend/models/usermodel";
 connect();
 
 export default async function VerifyEmail(req, res) {
-  try{
+    if (req.method === 'POST') {
+        try {
+            const { token } = req.body;
+            const user = await User.findOne({ verifyToken: token, verifyTokenExpiry: { $gt: Date.now() } });
 
-  }
-  catch(error){
-    return res.status(500).json({ error });
-  }
+            if (!user) {
+                return res.status(400).json({ error: "Invalid token" })
+            }
+
+            user.isVerified = true;
+            user.verifyToken = undefined;
+            user.verifyTokenExpiry = undefined;
+            await user.save();
+
+            return res.status(200).json({
+                message: "Email verified successfully",
+                success: true
+            })
+        }
+        catch (error) {
+            return res.status(500).json({ error });
+        }
+    }
 }
