309558bd7e3c6598bc80658e0b6a0ee1ca940683
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-11-11 23:16:16 +0500

    Removing spaces

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -37,8 +37,6 @@ export default async function Login(req, res) {
             });
 
             return res.status(200).json({ message: "Login Successful", token });
-
-
         }
         catch (error) {
             return res.status(500).json({ error: "Internal Server Error" });
