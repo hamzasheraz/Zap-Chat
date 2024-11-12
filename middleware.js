8ef7c5ff54752cb0e23996aeb6286190031fe9b0
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-11-12 18:25:40 +0500

    Token vrification added :middleware

--- middleware.js
+++ middleware.js
@@ -1,13 +1,26 @@
 import { NextResponse } from "next/server";
 import { cookies } from 'next/headers';
+import { jwtVerify } from 'jose';
 
 export async function middleware(request) {
   const path = new URL(request.url).pathname;
 
   const isPublicPath = path === "/login" || path === "/signup";
-
   const token = (await cookies()).get("token") || "";
 
+  if (token) {
+    try {
+      if (token.value.split('.').length === 3) {
+        await jwtVerify(token.value, new TextEncoder().encode(process.env.TOKEN_SECRET));
+      } else {
+        throw new Error("Token is malformed");
+      }
+    } catch (err) {
+      const cookieStore = cookies();
+      await cookieStore.set("token", "", { expires: new Date(0) });
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/", request.url));
   }
