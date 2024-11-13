65544876095b388c87bef5e549ac47effc152d97
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-11-13 15:30:03 +0500

    Settings click fixed

--- components/sidebar/options/index.jsx
+++ components/sidebar/options/index.jsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button"
 import { UserPlus, Settings } from 'lucide-react'
 import Link from 'next/link'
+import { useRouter } from "next/router"
 
 const Options = ({ setIsNewContactModalOpen }) => {
+    const router = useRouter();
     return (
         <div className="p-4 flex justify-between">
             <Button variant="outline" onClick={() => setIsNewContactModalOpen(true)}>
@@ -10,7 +12,9 @@ const Options = ({ setIsNewContactModalOpen }) => {
                 New Contact
             </Button>
             <Button variant="outline">
-                <Link href="/settings"><Settings className="h-5 w-5" /></Link>
+                <div className="h-5 w-5" onClick={() => router.push('/settings')}>
+                    <Settings />
+                </div>
             </Button>
         </div>
     )
