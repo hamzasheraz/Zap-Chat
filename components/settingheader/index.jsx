b971978927a83acc4bc6e9d21eaaee2e7c6da6fa
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-11-16 20:42:55 +0500

    Setting backword arrow:link fixed

--- components/settingheader/index.jsx
+++ components/settingheader/index.jsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button"
 import { CardTitle } from "@/components/ui/card"
 import { Zap, ArrowLeft } from 'lucide-react'
+import Link from "next/link"
 
 const Settingheader = () => {
     return (
         <div className="flex items-center justify-between">
             <Button variant="ghost" size="icon" className="mr-2">
-                <ArrowLeft className="h-6 w-6" />
+                <Link href="/" passHref>
+                    <ArrowLeft className="h-6 w-6" />
+                </Link>
             </Button>
             <CardTitle className="text-2xl font-bold flex items-center">
                 <Zap className="mr-2 h-6 w-6 text-yellow-500" />
